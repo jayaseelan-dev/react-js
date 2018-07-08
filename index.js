07f6a094b19113f4714e221bed5b38435e1ae5bf
jayaseelan-dev <s.jayaseelan16@gmail.com>
2018-07-08 15:32:28 +0530

    Hello world - Understanding ES6 and JSX

---
+++ index.js
@@ -0,0 +1,41 @@
+// https://reactjs.org/docs/add-react-to-a-website.html
+// https://reactjs.org/docs/react-without-jsx.html
+
+
+'use strict';
+
+const e = React.createElement;
+
+/***************** Without ES6 and without JSX  ************************/
+// const Greeting = React.createClass({
+// 	render: function() {
+// 		return e(
+// 			'h1',
+// 			null,
+// 			'Hello World!!!'
+// 		);
+// 	}
+// });
+
+// const domContainer = document.getElementById('app');
+// ReactDOM.render(e(Greeting), domContainer);
+
+/***************** With ES6 and without JSX  ************************/
+class Greeting extends React.Component {
+
+	handleChange() {
+		console.log('You clicked me!!!');
+	}
+
+	render() {
+		const greet = 'Good morning!!!';
+		return e(
+			'h1', // component
+			{ onClick: this.handleChange.bind(this) }, // props
+			`Hello, ${greet}` // ...children
+		)
+	}
+}
+
+const domContainer = document.getElementById('app');
+ReactDOM.render(e(Greeting), domContainer);
