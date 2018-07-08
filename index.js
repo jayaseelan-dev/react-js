9316babdd51e88ecfcaaf303674ffac809868461
jayaseelan-dev <s.jayaseelan16@gmail.com>
2018-07-08 22:36:04 +0530

    Implemented JSX with ES6

--- index.js
+++ index.js
@@ -1,41 +1,61 @@
 // https://reactjs.org/docs/add-react-to-a-website.html
 // https://reactjs.org/docs/react-without-jsx.html
 
-
 'use strict';
 
 const e = React.createElement;
 
 /***************** Without ES6 and without JSX  ************************/
-// const Greeting = React.createClass({
-// 	render: function() {
-// 		return e(
-// 			'h1',
-// 			null,
-// 			'Hello World!!!'
-// 		);
+const Greeting = React.createClass({
+	render: function() {
+		return e(
+			'h1',
+			null,
+			'Hello World!!!'
+		);
+	}
+});
+
+const domContainer = document.getElementById('app');
+ReactDOM.render(e(Greeting), domContainer);
+
+/***************** With ES6 and without JSX  ************************/
+// class Greeting extends React.Component {
+
+// 	handleChange() {
+// 		console.log('You clicked me!!!');
 // 	}
-// });
+
+// 	render() {
+// 		const greet = 'Good morning!!!';
+// 		return e(
+// 			'h1', // component
+// 			{ onClick: this.handleChange.bind(this) }, // props
+// 			`Hello, ${greet}` // ...children
+// 		)
+// 	}
+// }
 
 // const domContainer = document.getElementById('app');
 // ReactDOM.render(e(Greeting), domContainer);
 
-/***************** With ES6 and without JSX  ************************/
-class Greeting extends React.Component {
 
-	handleChange() {
-		console.log('You clicked me!!!');
-	}
+/***************** With ES6 and JSX  ************************/
+// class Greeting extends React.Component {
 
-	render() {
-		const greet = 'Good morning!!!';
-		return e(
-			'h1', // component
-			{ onClick: this.handleChange.bind(this) }, // props
-			`Hello, ${greet}` // ...children
-		)
-	}
-}
+// 	handleChange() {
+// 		console.log('You clicked me!!!');
+// 	}
 
-const domContainer = document.getElementById('app');
-ReactDOM.render(e(Greeting), domContainer);
+// 	render() {
+// 		const greet = 'Good morning!!!';
+// 		return (
+// 			<h1 onClick={this.handleChange.bind(this)}>
+// 				Hello, {greet}
+// 			</h1>
+// 		)
+// 	}
+// }
+
+// const domContainer = document.getElementById('app');
+// ReactDOM.render(<Greeting />, domContainer);
